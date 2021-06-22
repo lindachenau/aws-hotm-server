0b87b051fe9a3c96a591e19575362e7564516d19
Linda Chen <lindachenau@gmail.com>
2021-06-22 18:38:04 +1000

    Fixed a bug in adding token to the booking api

--- cancelbooking.js
+++ cancelbooking.js
@@ -62,23 +62,41 @@ const deleteBooking = async (bookingType, bookingId) => {
   const endPt = bookingType === 'therapist' ? '/bookings' : '/admin/bookings'
   const url = `${process.env.HOTM_BOOKING_API}${endPt}`
 
-  const config = {
-    method: 'get',
-    headers: {
-      "Content-Type": "application/json",
-      "Cache-Control": "no-cache, no-store, must-revalidate"
-    },
-    url: `${url}?id=${bookingId}`
-  }
-
   try {
+    const configToken = {
+      method: 'post',
+      headers: {"Content-Type": "application/json"},
+      url: process.env.API_TOKEN_LOGIN_URL,
+      data: {
+        "email" : process.env.REACT_APP_API_TOKEN_EMAIL,
+        "password" : process.env.REACT_APP_API_TOKEN_PASSWORD
+      }          
+    }
+  
+    const resultToken = await axios(configToken)
+    const apiToken = resultToken.data.jwt
+    console.log(apiToken)
+
+    const config = {
+      method: 'get',
+      headers: {
+        "Content-Type": "application/json",
+        "Cache-Control": "no-cache, no-store, must-revalidate",
+        "Authorization": `Bearer ${apiToken}`
+      },
+      url: `${url}?id=${bookingId}`
+    }
+  
     const result = await axios(config)
     const unpaid = result.data ? result.data.paid_amount === 0 : false //booking can return null if deleted
 
     if (unpaid) {
       const config = {
         method: 'delete',
-        headers: {"Content-Type": "application/json"},
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${apiToken}`
+        },
         url: url,
         data: {
           booking_id: bookingId
@@ -94,15 +112,15 @@ const deleteBooking = async (bookingType, bookingId) => {
     }
   } catch (err) {
     return Promise.reject({
+      url: url,
       booking_id: bookingId,
-      message: 'Booking has been cancelled'
+      message: `${err}. Booking has been cancelled`
     })
   }
 }
 
 module.exports.handler = (event, context, callback) => {
   const {targetId, rule, bookingType, bookingId} = event
-
   // Handle promise's fulfilled/rejected states
   deleteBooking(bookingType, bookingId).then((data) => {
     console.log(data.data)
